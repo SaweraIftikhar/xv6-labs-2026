2952024b3602c4dff8df50d3ddccb5f5e0b8b8fb
Sawera Iftikhar <s.iftikhar.31034@khi.iba.edu.pk>
2026-09-15 21:04:29 +0500

    implement memdump

--- user/memdump.c
+++ user/memdump.c
@@ -60,79 +60,77 @@ main(int argc, char *argv[])
 void
 memdump(char *fmt, char *data, int len)
 {
-  int i = 0;
-  int pos = 0;
+int i = 0;
+int pos = 0;
 
-  while (fmt[i] != '\0') {
-    char c = fmt[i];
+while (fmt[i] != '\0') {
+char c = fmt[i];
 
-    if (c == 'i') {
-      if (pos + 4 > len) {
-        printf("memdump: not enough data for 'i'\n");
-        return;
-      }
+if (c == 'i') {
+if (pos + 4 > len){ 
+printf("memdump: not enough data for 'i'\n");
+return;
+}
 
-      int n = *(int *)(data + pos);
-      printf("%d\n", n);
-      pos += 4;
-    }
+int n = *(int *)(data + pos);
+printf("%d\n", n);
+pos += 4;
+}
 
-    else if (c == 'p') {
-      if (pos + 8 > len) {
-        printf("memdump: not enough data for 'p'\n");
-        return;
-      }
+else if (c == 'p') {
+if (pos + 8 > len) {
+printf("memdump: not enough data for 'p'\n");
+return;
+}
 
-      unsigned long long n = *(unsigned long long *)(data + pos);
-      printf("%llx\n", n);
-      pos += 8;
-    }
+unsigned long long n = *(unsigned long long *)(data + pos);
+printf("%llx\n", n);
+pos += 8;
+}
 
-    else if (c == 'h') {
-      if (pos + 2 > len) {
-        printf("memdump: not enough data for 'h'\n");
-        return;
-      }
+else if (c == 'h') {
+if (pos + 2 > len) {
+printf("memdump: not enough data for 'h'\n");
+return;
+}
 
-      short n = *(short *)(data + pos);
-      printf("%d\n", n);
-      pos += 2;
-    }
+short n = *(short *)(data + pos);
+printf("%d\n", n);
+pos += 2;
+}
+else if (c == 'c') {
+if (pos + 1 > len) {
+printf("memdump: not enough data for 'c'\n");
+return;
+}
 
-    else if (c == 'c') {
-      if (pos + 1 > len) {
-        printf("memdump: not enough data for 'c'\n");
-        return;
-      }
+printf("%c\n", data[pos]);
+pos += 1;
+}
 
-      printf("%c\n", data[pos]);
-      pos += 1;
-    }
+else if (c == 's') {
+if (pos + 8 > len) {
+printf("memdump: not enough data for 's'\n");
+return;
+}
+char *ptr = *(char **)(data + pos);
+printf("%s\n", ptr);
+pos += 8;
+}
 
-    else if (c == 's') {
-      if (pos + 8 > len) {
-        printf("memdump: not enough data for 's'\n");
-        return;
-      }
+else if (c == 'S') {
+int j = pos;
 
-      char *ptr = *(char **)(data + pos);
-      printf("%s\n", ptr);
-      pos += 8;
-    }
+while (j < len && data[j] != '\0') {
+printf("%c", data[j]);
+j++;
+}
 
-    else if (c == 'S') {
-      int j = pos;
+printf("\n");
+pos = len;
+}
 
-      while (j < len && data[j] != '\0') {
-        printf("%c", data[j]);
-        j++;
-      }
-
-      printf("\n");
-      pos = len;
-    }
-
-    i++;
-  }
+i++;
+}
 }
 
