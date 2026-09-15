f2b1a6caadbadde9a0264be53c6b9c7fb99d033d
Sawera Iftikhar <s.iftikhar.31034@khi.iba.edu.pk>
2026-09-15 22:09:23 +0500

    assignment1: support regex in find name matching

--- user/find.c
+++ user/find.c
@@ -9,6 +9,45 @@ char *cmd[MAXARG];
 int cmd_count;
 int exec_mode;
 
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
+
+int
+match(char *re, char *text)
+{
+if(re[0] == '^')
+return matchhere(re+1, text);
+do{
+if(matchhere(re, text))
+return 1;
+}while(*text++ != '\0');
+return 0;
+}
+
+int
+matchhere(char *re, char *text)
+{
+if(re[0] == '\0')
+return 1;
+if(re[1] == '*')
+return matchstar(re[0], re+2, text);
+if(re[0] == '$' && re[1] == '\0')
+return *text == '\0';
+if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+return matchhere(re+1, text+1);
+return 0;
+}
+
+int
+matchstar(int c, char *re, char *text)
+{
+do{
+if(matchhere(re, text))
+return 1;
+}while(*text!='\0' && (*text++==c || c=='.'));
+return 0;
+}
+
 void
 run_command(char *path)
 {
@@ -55,7 +94,7 @@ p++;
 while (p > path && *(p - 1) != '/')
 p--;
 
-if (strcmp(p, name) == 0) {
+if (match(name, p)) {
 if (exec_mode)
 run_command(path);
 else
@@ -118,4 +157,3 @@ find(argv[1], argv[2]);
 
 exit(0);
 }
-
